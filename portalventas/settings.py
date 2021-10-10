612fd7c9cc69755f173769142080a2eb489b2638
jhonny <ismaelherz2001@gmail.com>
2021-10-10 11:29:29 -0600

    Declaracion de modelos

--- portalventas/settings.py
+++ portalventas/settings.py
@@ -49,6 +49,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'applications.Users',
+    'applications.PaginaVenta',
+    'applications.Producto',
+    'applications.Proveedor',
+    'applications.Compra',
 ]
 
 MIDDLEWARE = [
@@ -129,11 +134,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+#modelo de usuario
+AUTH_USER_MODEL = 'Users.Usuario'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# Archivos estaticos
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+
+# Archivos de media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
