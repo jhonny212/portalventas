c7433f0cade23b31bfeb17196432d9c701acd9c2
jhonny <ismaelherz2001@gmail.com>
2021-11-05 09:56:06 -0600

    Correccion de redireccionamiento

--- applications/Producto/managers.py
+++ applications/Producto/managers.py
@@ -3,18 +3,17 @@ from .models import PaginaVentas
 class ProductoServicioManager(models.Manager):
 
     def get_productos_by_user(self,user_id,site=''):
-        sql = """
+        sql = f"""
         select p.nombre,p.foto,p.precio,p.cantidad,p.descripcion,p.id,
         pv.id,pv.nombre_sitio, 
         ps.usuario_id 
         from "Producto_productoservicio" as p INNER JOIN "PaginaVenta_paginaventas" as pv
         ON p.id_pagina_ventas_id = pv.id
         INNER JOIN "PaginaVenta_suscripciones" as ps ON ps.id_pagina_ventas_id = pv.id
-        where ps.usuario_id=%s and p.nombre LIKE %s 
+        where ps.usuario_id={user_id} and p.nombre LIKE '%{site}%' 
         """
-        x=[user_id,f"%{site}%"]
         with connection.cursor() as cursor:
-            cursor.execute(sql,x)
+            cursor.execute(sql)
             row = cursor.fetchall()
         return row
     
